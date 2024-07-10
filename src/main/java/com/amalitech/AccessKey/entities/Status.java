9628d6eb93bb65bef1ff1b8fd5a7f0bceaea0ebd
heyEdem <akatoaugustine@gmail.com>
2024-07-10 17:01:13 +0000

    feat: required entites

---
+++ src/main/java/com/amalitech/AccessKey/entities/Status.java
@@ -0,0 +1,7 @@
+package com.amalitech.AccessKey.entities;
+
+public enum Status {
+    ACTIVE,
+    REVOKED,
+    EXPIRED
+}
---
+++ src/main/java/com/amalitech/AccessKey/entities/User.java
@@ -0,0 +1,52 @@
+package com.amalitech.AccessKey.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Roles roles;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private boolean isEnabled;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
