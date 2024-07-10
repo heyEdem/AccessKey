98e2c8ef11558ba8d5e8dd2e005bd2ef48c9f6fa
heyEdem <akatoaugustine@gmail.com>
2024-07-10 15:57:37 +0000

    first

---
+++ src/main/java/com/amalitech/AccessKey/AccessKeyApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.AccessKey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessKeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalitech/AccessKey/AccessKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.AccessKey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
