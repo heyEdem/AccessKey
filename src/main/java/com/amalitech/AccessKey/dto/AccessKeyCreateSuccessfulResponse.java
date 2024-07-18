176850a0dc487cad41ffcd1c28246d67ac1f498c
heyEdem <akatoaugustine@gmail.com>
2024-07-18 18:24:20 +0000

    quick fix to access key service

--- src/main/java/com/amalitech/AccessKey/dto/AccessKeyCreateSuccessfulResponse.java
+++ src/main/java/com/amalitech/AccessKey/dto/AccessKeyCreateSuccessfulResponse.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 public record AccessKeyCreateSuccessfulResponse (
         String message,
         String code,
-        LocalDate expiry
+        LocalDate expiry,
+        String status
 ){
 }
