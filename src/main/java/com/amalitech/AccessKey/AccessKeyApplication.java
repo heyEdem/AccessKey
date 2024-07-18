334ea7516ebebda7ac98e41e8f82c17f30a375f8
heyEdem <akatoaugustine@gmail.com>
2024-07-18 18:59:53 +0000

    dotenv setup

--- src/main/java/com/amalitech/AccessKey/AccessKeyApplication.java
+++ src/main/java/com/amalitech/AccessKey/AccessKeyApplication.java
@@ -1,10 +1,20 @@
 package com.amalitech.AccessKey;
 
+import jakarta.annotation.PostConstruct;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccessKeyApplication {
+	@PostConstruct
+	public void init() {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.mail.host", dotenv.get("MAIL_HOST"));
+		System.setProperty("spring.mail.port", dotenv.get("MAIL_PORT"));
+		System.setProperty("spring.mail.username", dotenv.get("MAIL_USERNAME"));
+		System.setProperty("spring.mail.password", dotenv.get("MAIL_PASSWORD"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessKeyApplication.class, args);
